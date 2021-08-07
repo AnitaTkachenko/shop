f174b3e41d4d915932e7eb5e30e52d0fe0f70a3f
Anita Tkachenko <anitaguskova3@gmail.com>
2021-08-08 00:12:44 +0600

    Скорректировала файл ProductController.java

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,8 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -11,5 +18,25 @@ public class ProductController {
     public ProductController(ProductService productService) { this.productService = productService; }
 
     @GetMapping
-    public String start(){ return "start"; }
+    public String start(Model model){
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "start"; }
+
+    @GetMapping("addProduct")
+    public String getAddProduct(Model model){
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "addProduct";
+    }
+    @PostMapping("addProducts")
+    public String addProducts(@RequestParam String name,
+                              @RequestParam int price,
+                              @RequestParam String description,
+                              @RequestParam String manufacturer,
+                              @RequestParam String category){
+        Product product = new Product(name, price, description, manufacturer, category);
+        productService.save(product);
+        return "addProduct";
+    }
 }
--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -12,17 +12,19 @@ public class Product {
     @Id
     private String id = UUID.randomUUID().toString();
     private String name;
+    private int price;
     private String description;
     private String manufacturer;
     private String category;
     private int quantity;
 
 
-    public Product(String name, String description, String manufacturer, String category, int quantity) {
+    public Product(String name,int price, String description, String manufacturer, String category) {
         this.name = name;
+        this.price = price;
         this.description = description;
         this.manufacturer = manufacturer;
         this.category = category;
-        this.quantity = quantity;
+        this.quantity = 1;
     }
 }
